ec8cd755ae951da8ba931cd9e715ba9b1e6b72bd
AshiqurRahaman02 <ashiqur999999@gamil.com>
2024-10-17 15:51:55 +0530

    Commit 2: Added multiplyByNine function

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ function multiplyByFive(num) {
     return num * 5;
 }
 
-module.exports = { multiplyByFive };
+// Function to multiply a number by 9
+function multiplyByNine(num) {
+    return num * 9;
+}
+
+module.exports = { multiplyByFive, multiplyByNine };
